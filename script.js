96450a0b12c2c6df92befa7d697f46c6cda22d9b
Mari-Artho <mari.artho@medieinstitutet.se>
2021-12-01 16:47:14 +0100

    Success to login.

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+const button = document.getElementsByTagName("button")[0];
+button.addEventListener("click", login());
+
 function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
+  const loginSuccess = document.getElementById("loginSuccess");
+  const loginFail = document.getElementById("loginFail");
   if (username == "janne" && password == "test") {
-    //alert("Welcome!");
-    //return false;
-    let welcome = document.querySelector("header");
-    welcome.innerText = "Welcome!!";
+    loginSuccess.textContent = "Welcome!! Your name is" + username;
+    console.log("ログインできました");
   } else {
-    alert("Your are login fail, please try again.");
+    loginFail.textContent = "Login fail, please try again.";
+    console.log("ログインできませんでした");
   }
 }
+
+//test
+button.addEventListener("click", onClickBtn());
+
+function onClickBtn() {
+  console.log("ボタンがクリックされました。");
+  console.log(username.value);
+}
+
+//Save to local storage
+localStorage.setItem("username", username.value);
+localStorage.setItem("password", password.value);
